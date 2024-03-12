912a1ddb7b04241e8a08114e4023598c9354163b
Chandupriya-411 <163101950+Chandupriya-411@users.noreply.github.com>
2024-03-12 14:15:28 +0530

    Create Custom.java

---
+++ Custom.java
@@ -0,0 +1,25 @@
+public class CustomValidationException extends Exception {
+    public CustomValidationException(String message) {
+        super(message);
+    }
+}
+
+
+public class Example {
+    public static void main(String[] args) {
+        int number = -5;
+        
+        try {
+            validateNumber(number);
+            System.out.println("Number is valid: " + number);
+        } catch (CustomValidationException e) {
+            System.out.println("CustomValidationException occurred: " + e.getMessage());
+        }
+    }
+    
+    public static void validateNumber(int number) throws CustomValidationException {
+        if (number < 0) {
+            throw new CustomValidationException("Number cannot be negative");
+        }
+    }
+}
